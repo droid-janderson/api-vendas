7542712557c43b030878b5754314c2f6aea651b4
droid-janderson <jandersongit@gmail.com>
2023-11-09 05:15:00 -0300

    config routes products

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,11 +1,8 @@
-import { Router } from "express";
+import productsRouter from '@modules/products/routes/products.routes';
+import { Router } from 'express';
 
 const routes = Router();
 
-routes.get('/', (req, res) => {
-  return res.json({
-    message: 'Welcome to the application!'
-  });
-})
+routes.use('/products', productsRouter);
 
 export default routes;
